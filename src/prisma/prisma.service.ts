50d03b90e1bfa1301d77f95231c2b1d0dcd9765b
lolmatina <almat.orazali02@gmail.com>
2025-04-23 23:53:10 +0500

    prisma import fix

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from 'prisma/app/generated/prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
